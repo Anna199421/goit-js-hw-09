9e7ffe3ce71610da1c6e81c17ac275addd6d1efb
Anna199421 <111239829+Anna199421@users.noreply.github.com>
2023-02-04 15:46:41 +0200

    fff

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+const body = document.body;
+const startBtn = body.querySelector('[data-start]');
+const closeBtn = body.querySelector('[data-stop]');
 
+let intervalId = null;
+
+onLoad();
+
+startBtn.addEventListener('click', onStartClick);
+closeBtn.addEventListener('click', onStopClick);
+
+function onStartClick({ target }) {
+  intervalId = setInterval(
+    () => (body.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+  target.disabled = true;
+  closeBtn.disabled = false;
+}
+
+function onStopClick({ target }) {
+  clearInterval(intervalId);
+  target.disabled = true;
+  startBtn.disabled = false;
+  localStorage.setItem('color', body.style.backgroundColor);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function bgColorSet() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function onLoad() {
+  closeBtn.disabled = 'disabled';
+  body.style.backgroundColor = localStorage.getItem('color');
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,88 @@
+import flatpickr from 'flatpickr';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'flatpickr/dist/flatpickr.min.css';
+
+let selectedTime = null;
+let intervalId = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    getTime.call(options, selectedDates[0]);
+  },
+};
+
+const inputEl = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const daysOutput = document.querySelector('[data-days]');
+const hoursOutput = document.querySelector('[data-hours]');
+const minutesOutput = document.querySelector('[data-minutes]');
+const secondsOutput = document.querySelector('[data-seconds]');
+
+startBtn.disabled = true;
+
+flatpickr(inputEl, options);
+startBtn.addEventListener('click', onStartClick);
+
+function getTime(selectedDate) {
+  if (this.defaultDate > selectedDate) {
+    Notify.failure('Please choose a date in the future', {
+      clickToClose: true,
+    });
+    return;
+  }
+
+  selectedTime = selectedDate;
+  startBtn.disabled = false;
+}
+
+function onStartClick() {
+  setTimer();
+  intervalId = setInterval(setTimer, 1000);
+  startBtn.disabled = true;
+}
+
+function setTimer() {
+  const currentTime = Date.now();
+  const deltaTime = selectedTime - currentTime;
+
+  if (deltaTime < 1000) {
+    outputTimerValue(convertMs(deltaTime));
+    clearInterval(intervalId);
+    return;
+  }
+  outputTimerValue(convertMs(deltaTime));
+}
+
+function outputTimerValue({ days, hours, minutes, seconds }) {
+  daysOutput.textContent = days;
+  hoursOutput.textContent = hours;
+  minutesOutput.textContent = minutes;
+  secondsOutput.textContent = seconds;
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
